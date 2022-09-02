d6fd61b1cb33d1aed2677e50525b483aea07f8e1
Наталья <109668015+andre19071@users.noreply.github.com>
2022-09-02 21:56:52 +0700

    task66

--- task66/Program.cs
+++ task66/Program.cs
@@ -2,3 +2,23 @@
 // сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM > numberN)
+{
+    Console.WriteLine("Число M не может быть больше N");
+}
+else
+{
+    int sum = PrintNumbersToN(numberN, numberM);
+    Console.WriteLine(sum);
+}
+int PrintNumbersToN(int n, int m)
+{
+    if (n < m) return 0;
+    int sum = PrintNumbersToN(n - 1, m);
+    return sum += n;
+}
